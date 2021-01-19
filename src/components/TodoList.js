f44f864ef9036ec0c02aace7f9d706b5534b6b57
Ovidiu Toma <ovidiutoma876@gmail.com>
2021-01-19 17:58:51 +0200

    App complete + 1/2 of css done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo'
 
 function TodoList(){
 
@@ -13,12 +14,38 @@ function TodoList(){
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos);
+    };
+
+    const updateTodo = (todoId, newValue)=> {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item ))
+        );
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    }
+
+    const completeTodo = id =>{
+        let updatededTodos = todos.map(todo => {
+            if (todo.id === id ) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatededTodos);
     }
 
     return(
         <div>
-            <h1>Whats poppin baby</h1>
+            <h1>Your Todo List in React</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 }
